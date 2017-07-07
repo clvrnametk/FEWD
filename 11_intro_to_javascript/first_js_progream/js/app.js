1b1d170c2a53f8910baf567715926f5f26d77d4b
Mike Morales <mike.morales.ny@gmail.com>
2017-07-06 20:16:46 -0400

    First js app

---
+++ 11_intro_to_javascript/first_js_progream/js/app.js
@@ -0,0 +1,14 @@
+/* Variables */
+var a = parseInt( prompt("Ener a numner") );
+var b = parseInt( prompt("Ener a another numner") );
+
+var course = prompt("What is the name of your course?");
+
+/* Console */
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+console.log (course + " is " + course.length + " characters long");
+console.log (course + " is cool");
