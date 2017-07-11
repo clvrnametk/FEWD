26f33602976dd1b922791da3ab60a9502dcf7cb7
Mike Morales <mike.morales.ny@gmail.com>
2017-07-11 17:55:56 -0400

    Moved temp conversion into a variable

--- 11_intro_to_javascript/assignment/03_Temp_Converter/js/tempConverter.js
+++ 11_intro_to_javascript/assignment/03_Temp_Converter/js/tempConverter.js
@@ -1,20 +1,22 @@
 /* Variables */
 var tempNum = parseInt( prompt("What temperature do you want convert?" + "\n" + "Enter only numbers"))
 var tempConvert = prompt("What temperature scale would you like to convert that temperature to? Enter \"F to C\" or \"C to F\"")
+var tempConvertF = parseInt (((tempNum * 9) / 5) + 32) /* ParseInt to remove decimal pointsa from temp */
+var tempConvertC = parseInt (((tempNum - 32) * 5) / 9) /* ParseInt to remove decimal pointsa from temp */
 
 
 /* Print to Console */
 if  (
   tempConvert == "C to F"
     ) {
-  console.log(tempNum + "\xB0C converts to " + (((tempNum * 9) / 5) + 32) + "\xB0F");
+  console.log(tempNum + "\xB0C converts to " + tempConvertF + "\xB0F");
 } else if (
           tempConvert == "F to C"
           ) {
-  console.log(tempNum + "\xB0F converts to " + (((tempNum - 32) * 5) / 9) + "\xB0C");
+  console.log(tempNum + "\xB0F converts to " + tempConvertC + "\xB0C");
 } else if (
           isNaN(tempNum) &&
-          tempConvert != "F to C" ||
+          tempConvert != "F to C" || /* @HARI: I couldn't figure out how to make this part work well */
           tempConvert != "C to F"
           ){
   console.log("Looks like you didn't enter a number for the temperature" + "\n" + "You also didn't enter \"F to C\" or \"C to F\" for conversion." + "\n" + "Please refresh and try again.");
---
+++ 11_intro_to_javascript/assignment/04_Calculator/js/calculator.js
@@ -0,0 +1,4 @@
+/* Variables */
+
+
+/* Print to Console */
