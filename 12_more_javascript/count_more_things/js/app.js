ddcd5e761b66250e84b0218962661e81875acbc6
Mike Morales <mike.morales.ny@gmail.com>
2017-07-11 20:21:18 -0400

    Finished some for loop things

---
+++ 12_more_javascript/count_more_things/js/app.js
@@ -0,0 +1,31 @@
+console.log("Count to 20!!");
+
+for (number = 1; number <= 20; number++) {
+  console.log(number);
+}
+
+console.log("Finished!!");
+
+console.log("Count even to 50!!");
+
+for (number = 2; number <= 50; number = number + 2) {
+  console.log(number);
+}
+
+console.log("Finished!!");
+
+console.log("Count down from 10!!");
+
+for (number = 10; number > 0; number = number - 1) {
+  console.log(number);
+}
+
+console.log("Finished!!");
+
+console.log("Count down from 50 using!!");
+
+for (number = 50; number >0.00001; number = number / 2) {
+  console.log(number);
+}
+
+console.log("Finished!!");
